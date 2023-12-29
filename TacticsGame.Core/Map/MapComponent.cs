d825bfe9a6c6c87cce1282a1aec3f42cf6aebf8e
zergar5 <artem_kozhanov@mail.ru>
2023-12-29 12:25:14 +0700

    Добавьте файлы проекта.

---
+++ TacticsGame.Core/Map/MapComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace TacticsGame.Core.Map;
+
+public class MapComponent
+{
+    public Map Map { get; }
+    public Size TileSize { get; }
+
+    public MapComponent(Map map)
+    {
+        Map = map;
+        TileSize = new Size();
+    }
+}
---
+++ TacticsGame.Core/Map/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace TacticsGame.Core.Map;
+
+public class Tile
+{
+    public Point Location { get; }
+    public TileType Type { get; }
+}
+
+public enum TileType
+{
+    Free,
+    Obstacle,
+    Abyss
+}
