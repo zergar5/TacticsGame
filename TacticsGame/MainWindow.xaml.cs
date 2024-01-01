3b7ff21ee41525e9a740ef2efa18b2c4d7e6b0e7
volyas <115228295+volyas@users.noreply.github.com>
2024-01-02 00:06:13 +0800

    Начало панельке положено.

--- TacticsGame/MainWindow.xaml.cs
+++ TacticsGame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SharpGL.WPF;
+using TacticsGame;
 
 namespace TacticsGame
 {
@@ -24,21 +26,56 @@ namespace TacticsGame
         public MainWindow()
         {
             InitializeComponent();
+            //var units = new List<Image>();
+            //unitsList.ItemsSource = units;
+            var path = "C:\\Programming\\UNI_Projects\\TacticsGame\\TacticsGame\\UIcons";
+            string[] imagesPath = Directory.GetFiles(path);
+            Image img;            
+
+            var units = new List<Unit>() {
+                new Unit(path + "\\hero1.jpg"),
+                new Unit(path + "\\hero1.jpg"),
+                new Unit(path + "\\hero2.jpg"),
+                new Unit(path + "\\hero1.jpg"),
+                new Unit(path + "\\hero2.jpg")};
+
+
+            foreach (Unit unit in units)
+            {
+                img = new Image();
+                img.Height = 80;
+                img.Source = unit.Image;
+                unitsList.Children.Add(img);
+            }
+
+            //unitsList.Items.Refresh();
+
+            //GameData gameData = new GameData();
+            //gameData.Hero1Image = "C:\\Programming\\UNI_Projects\\TacticsGame\\TacticsGame\\UIcons\\hero1.jpg";            
+            //gameData.Hero1HP = 50;
+            //gameData.Hero2Image = "C:\\Programming\\UNI_Projects\\TacticsGame\\TacticsGame\\UIcons\\hero2.jpg";
+            //gameData.Hero2HP = 90;
+            //gameData.Hero3Image = "hero3.png";
+            //gameData.Hero3HP = 100;
+            //gameData.HPWidth = 100;
+
+            //GamePanel.DataContext = gameData;
+
         }
 
         private void OpenGLDraw(object sender, OpenGLRoutedEventArgs args)
         {
-            throw new NotImplementedException();
+           
         }
 
         private void OpenGLInitialized(object sender, OpenGLRoutedEventArgs args)
         {
-            throw new NotImplementedException();
+           
         }
 
         private void OpenGLControlResized(object sender, OpenGLRoutedEventArgs args)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
---
+++ TacticsGame/Unit/Unit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TacticsGame
+{
+    public class Unit
+    {
+        public String Name { get; }
+        public int HP { get; set; }
+        public ImageSource Image { get; }
+
+        public Unit(string path)
+        {
+            Name = Path.GetFileName(path);
+            Image = BitmapFrame.Create(new Uri(path));
+        }
+
+    }
+}
